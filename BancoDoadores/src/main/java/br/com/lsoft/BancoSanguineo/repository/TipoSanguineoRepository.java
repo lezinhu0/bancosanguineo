bdcd0d8822054bd3d92165e1bdc66f147edb13a4
leand <leandro.rufma@gmail.com>
2021-05-19 19:07:05 -0300

    .

--- BancoDoadores/src/main/java/br/com/lsoft/BancoSanguineo/repository/TipoSanguineoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lsoft.BancoSanguineo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.lsoft.BancoSanguineo.model.TipoSanguineo;
-
-public interface TipoSanguineoRepository extends CrudRepository<TipoSanguineo, String>{
-
-}
